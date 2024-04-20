9ff0218c356475fdcfedc7e213f437d4c40ed092
Estefanyq19 <estefanyq1908@gmail.com>
2024-04-20 15:22:22 -0600

    Correción de request

--- app/Http/Requests/ActualizarDepartamento.php
+++ app/Http/Requests/ActualizarDepartamento.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SaveInfoMunicipio extends FormRequest
+class ActualizarDepartamento extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,10 +24,6 @@ class SaveInfoMunicipio extends FormRequest
     public function rules()
     {
         return [
-            'departamento_id'=>array(
-                'required',
-                'int'
-            ),
             'nombre'=>array(
                 'required'
             )
